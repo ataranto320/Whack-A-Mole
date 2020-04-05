55f439fd022f42cc689785ed381c5097f9ff5771
Austin T <austin.taranto@gmail.com>
2020-04-05 13:32:06 -0400

    random hole function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,22 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+let lastHole;
 
 function randomTime(min, max) {
     return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes) {
+    // console.log(holes.length);
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole === lastHole) {
+        console.log("Nope");
+        return randomHole(holes);
+    }
+    // console.log(hole);
+
+    lastHole = hole;
+    return hole;
 }
